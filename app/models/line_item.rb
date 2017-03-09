861f238a3f0142b2c3652b1b2a4b2100e6c50058
Ernest Banasiak <ernasiak@gmail.com>
2017-03-09 18:16:44 +0000

    lab 9

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,10 @@
 class LineItem < ApplicationRecord
-  def total_price
-  product.price * quantity
-  end
-  belongs_to :product
-  belongs_to :cart
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
+  
+  belongs_to :cart 
+
+  def total_price 
+  product.price * quantity 
+  end 
 end
